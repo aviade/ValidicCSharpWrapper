793f16fbe153f566ff22c0f333888c7b462ab98d
Aviad Ezra <aviadezra@gmail.com>
2017-02-09 16:13:18 -0800

    Update MainModel.cs

--- ValidicCSharpApp/Models/MainModel.cs
+++ ValidicCSharpApp/Models/MainModel.cs
@@ -13,8 +13,8 @@ namespace ValidicCSharpApp.Models
             {
                 new OrganizationAuthenticationCredentials
                 {
-                    OrganizationId = "55dcc3a9623865001100000b",
-                    AccessToken = "b1406efd1f8b50fb1190a3faa751a73a73838c7cc6012ba3119bead30f9596da"
+                    OrganizationId = "51aca5a06dedda916400002b",
+                    AccessToken = "ENTERPRISE_KEY"
                 },
             };
         }
